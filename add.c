f95c62ac2fe689ab552f61398a530da82eb06768
Bhoomika <bhoomika.jalandhra@gmail.com>
2025-06-08 10:29:09 +0530

    Add basic c program to add two numbers

---
+++ add.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main()
+{
+    int a,b,s;
+    printf("Enter two numbers that you want to add: ");
+    scanf("%d %d",&a,&b);
+    s=a+b;
+    printf("Sum of %d and %d is %d",a,b,s);
+}
